fda257e68a16405280632c690d6f70357022ffc7
Dmitry Makovey <dmakovey@yahoo.com>
2020-10-14 10:41:30 -0600

    fix up vcard

--- extract_vcard.py
+++ extract_vcard.py
@@ -1,6 +1,7 @@
 #!/bin/env python
 
 from xml.etree.ElementTree import ElementTree
+import xml.etree.ElementTree as ET
 
 import sys
 import json
@@ -8,23 +9,14 @@ import os.path
 import os
 import argparse
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Extract VCARD from CardDav Export')
-    parser.add_argument('filename', 
-                    help='XML file with addressbook entries exported via CardDav')
-    parser.add_argument('--no-individual', dest='individual', action="store_false")
-    parser.add_argument('--write-individual', dest='individual', action="store_true", default=True)
-    parser.add_argument('--no-full', dest='full', action="store_false")
-    parser.add_argument('--write-full', dest='full', action="store_true", default=False)
+def env(var_name):
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        return None
 
-    parser.add_argument('--vcard-dir', default='./vcard')
-
-    args = parser.parse_args()
-
-    addressbook_filename=args.filename
-
-    mydoc = ElementTree(file=addressbook_filename)
-    addressbook_name=addressbook_filename.split('/')[-1].split('.')[0]
+def parse_addressbook(addressbook_name, addressbook_xml, args):
+    mydoc = ET.fromstring(addressbook_xml)
     try:
         os.makedirs(os.path.join(args.vcard_dir,addressbook_name))
     except FileExistsError:
@@ -56,4 +48,64 @@ if __name__ == "__main__":
                 full_addressbook.write("\n".join(card_entry))
 
         
+
+if __name__ == "__main__":
+    env_vars=['CARDDAV_URI','CARDDAV_USER','CARDDAV_PASSWORD','CARDDAV_ADDRESSBOOKS']
+    env_defaults={}
+    for v in env_vars:
+        if v=='CARDDAV_ADDRESSBOOKS':
+            res=env(v)
+            if res:
+                env_defaults[v]=res.split()
+            else:
+                env_defaults[v]=res
+        else:
+            env_defaults[v]=env(v)
+    parser = argparse.ArgumentParser(description='Extract VCARD from CardDav Export')
+    parser.add_argument('--carddav-uri', default=env_defaults['CARDDAV_URI'])
+    parser.add_argument('--carddav-user', default=env_defaults['CARDDAV_USER'])
+    parser.add_argument('--carddav-password', default=env_defaults['CARDDAV_PASSWORD'])
+    parser.add_argument('--carddav-addressbook', dest='carddav_addressbooks', metavar='CARDDAV_ADDRESSBOOK', action='append', default=env_defaults['CARDDAV_ADDRESSBOOKS'], help='use multiple entries for multiple addressbooks')
+    parser.add_argument('--netrc-file', default=os.environ['HOME'])
+    parser.add_argument('--filename', 
+                    help='XML file with addressbook entries exported via CardDav')
+    parser.add_argument('--no-individual', dest='individual', action="store_false")
+    parser.add_argument('--write-individual', dest='individual', action="store_true", default=True)
+    parser.add_argument('--no-full', dest='full', action="store_false")
+    parser.add_argument('--write-full', dest='full', action="store_true", default=False)
+
+    parser.add_argument('--vcard-dir', default='./vcard')
+
+    args = parser.parse_args()
+
+    if args.carddav_uri and args.carddav_addressbooks:
+        # we have remote calendars to fetch
+        username=args.carddav_user
+        password=args.carddav_password
+        if args.netrc_file:
+            import netrc
+            import urllib.parse
+            uri=urllib.parse.urlparse(args.carddav_uri)
+            n=netrc.netrc(args.netrc_file)
+            (username,account,password)=n.authenticators(uri.hostname)
+        # start fetching...
+        import requests
+        my_headers={
+            "Content-Type": "text/xml",
+            "Depth": "1",
+        }
+        my_data="<propfind xmlns='DAV:'><prop><address-data xmlns=\"urn:ietf:params:xml:ns:carddav\"/></prop></propfind>"
+        for addressbook in args.carddav_addressbooks:
+            url="{uri}/{addressbook}".format(uri=args.carddav_uri,addressbook=addressbook)
+            r=requests.request('PROPFIND',url, 
+                    headers=my_headers,
+                    data=my_data,
+                    auth=(username,password))
+            parse_addressbook(addressbook, r.text, args)
+    elif args.filename:
+        addressbook_filename=args.filename
+        addressbook_name=addressbook_filename.split('/')[-1].split('.')[0]
+        with open(addressbook_filename, 'r') as f:
+            parse_addressbook(addressbook_name, f.read(), args)
+
 
